cffa78bada33bd778351cd5b58dcc3a890bd1e2f
Suegathh <suzzainnegathu@gmail.com>
2025-04-25 23:36:09 +0300

    add allowed hosts

--- bookings/settings.py
+++ bookings/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = 'django-insecure-mv3tryqkl+=ejl4xz9yv_3xk46x5a1)s_0(*f_%tiwpx)q+2=0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'travel-buddy-7g6f.onrender.com', 
+    'localhost', '127.0.0.1', 
+    "https://travel-buddy-frontend-xi.vercel.app",
+    'http://localhost:5173'
+    ]
+
 
 
 # Application definition
